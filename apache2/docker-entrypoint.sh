c0e3072278fa32ca18b94788d75629868ef0c863
Iain Hadgraft <iain.hadgraft@duke.edu>
2017-04-09 08:04:57 -0400

    Initial commit

---
+++ apache2/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -z "$PHPFPM_HOST" ] ; then
+    mv /etc/optional/php-fpm.conf /etc/apache2/conf.d/php-fpm.conf
+fi
+
+if [ ! -d "$DOCUMENT_ROOT" ] ; then
+    mkdir -p "$DOCUMENT_ROOT"
+fi
+
+if [ ! -d "$SERVER_ROOT" ] ; then
+    mkdir -p "$SERVER_ROOT"
+fi
+
+ln -s /var/log/apache2 "$SERVER_ROOT"/logs
+ln -s /usr/lib/apache2 "$SERVER_ROOT"/modules
+ln -s /run/apache2 "$SERVER_ROOT"/run
+
+"$@"
